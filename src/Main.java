25cf1c8fc31cebfc61d0be1cef36ec4c39023afa
Andrei <calipso063@gmail.com>
2024-06-29 22:14:11 +0400

    Стартовый проект

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello World!");
+        String post = scanner.nextLine();
+        System.out.print("Hi");
+
+
+    }
+
+}
